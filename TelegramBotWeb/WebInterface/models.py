7c70215de073355798d830ba0809062f11ed93c7
pioner951 <Romashka951@gmail.com>
2021-07-29 17:30:49 +0500

    Создание изменненноё модели апользователя

--- TelegramBotWeb/WebInterface/models.py
+++ TelegramBotWeb/WebInterface/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Department(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField("Название отдела", max_length=255)
+
+    def __str__(self):
+        return self.name
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.OneToOneField(Department, on_delete=models.CASCADE)
+
+
