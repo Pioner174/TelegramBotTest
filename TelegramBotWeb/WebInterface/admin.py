fa37ee7edd99f6cc2dadab8f66caa2bfe00fe366
pioner951 <Romashka951@gmail.com>
2021-07-30 14:46:14 +0500

    Создана модель, настроена админка для редактирова

--- TelegramBotWeb/WebInterface/admin.py
+++ TelegramBotWeb/WebInterface/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Department, Locality, Employee
 
 # Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Locality)
+class LocalitytAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(Employee)
+class EmployeetAdmin(admin.ModelAdmin):
+    list_display = ['user','name','surname','middle_name','position','department','locality','telephone_r','mobile_phone_r','telephone_comp','telephone_telegram']
+    list_editable = ['telephone_r','mobile_phone_r','telephone_comp','telephone_telegram']
+    list_filter = ['telephone_r','mobile_phone_r','telephone_comp','telephone_telegram']
+    
--- TelegramBotWeb/WebInterface/models.py
+++ TelegramBotWeb/WebInterface/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Department(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField("Название отдела", max_length=255)
+    def __str__(self):
+        return self.name
 
+class Locality(models.Model):
+    name = models.CharField("Населённый пункт", max_length=255)
     def __str__(self):
         return self.name
 class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.OneToOneField(Department, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True,null=True,)
+    name = models.CharField("Имя", max_length=255)
+    surname = models.CharField("Фамилия",max_length=255)
+    middle_name = models.CharField("Отчество", max_length=255)
+    position = models.CharField("Должность", max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, verbose_name="Департамент",blank=True,null=True,)
+    locality = models.ForeignKey(Locality, on_delete=models.SET_NULL, verbose_name="Локация",blank=True,null=True,)
+    telephone_r = PhoneNumberField("Прямой телефон рабочий", blank=True)
+    mobile_phone_r = PhoneNumberField("Мобильный рабочий телефон", blank=True)
+    telephone_comp = PhoneNumberField("Внутренний телефон в компании", blank=True)
+    telephone_telegram = PhoneNumberField("Телефон привязанный к телеге", blank=True)
+
 
 
