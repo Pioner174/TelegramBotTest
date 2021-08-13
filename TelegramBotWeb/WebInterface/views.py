d55e9819591aad27e1726b7898f657f743fc61e7
pioner951 <Romashka951@gmail.com>
2021-08-13 16:12:19 +0500

    Настроена передача значений, но она не очень работ

--- TelegramBotWeb/WebInterface/views.py
+++ TelegramBotWeb/WebInterface/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 
-from .forms import LoginForm, PeopleSelect, MessSelect#, BotStatus
+from .forms import LoginForm, PeopleSelect, MessSelect
+from .models import *
+#, BotStatus
 # from ...PythonTelegramBot.UpdaterBot import *
 
 def user_login(request):
@@ -37,8 +39,14 @@ def mailing(request):
             for i in employee_tid:
                 str_id.append(i['t_user_id'])
             for person_in_group in cd['group']:
-                pass
-            pass
+                 for rel in Memberships.objects.filter(group = person_in_group).select_related():
+                    for t_id in  Employee.objects.filter(pk=rel.person_id).values('t_user_id'):
+                        str_id.append(t_id['t_user_id'])
+            str_id = set(str_id)
+            str_name = Employee.objects.filter(t_user_id__in = str_id)
+            message_form = MessSelect(initial={"text_choice":str_name})
+            
+            return render(request,'WebInterface/newsletter.html', {'form': malling_form, 'mess_form':message_form})
 
     else:
         malling_form = PeopleSelect()
